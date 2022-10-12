e9ae5f18bc55faa2b311a8fd20d7733a949ba7e5
estherolagunju <estherolagunju2000@gmail.com>
2022-10-12 15:33:51 +0100

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 
 
+const cards = document.querySelectorAll(".card");
+let matchedParis = 0;
+let cardOne, cardTwo;
+let disableDeck = false;
+
+
+
+function flipCard(){
+    console.log('flipCard was executed');
+}
+
+
+
+
+function shuffleCards(){
+    let matchedParis = 0;
+let cardOne = cardTwo ="";
+let disableDeck = false;
+
+let arr = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
+arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+
+cards.forEach((card, i) => {
+    card.classList.remove('flip');
+    let imgTag = card.querySelector(".back-view img");
+    imgTag.src = `images/img-${arr[i]}.png`;
+    card.addEventListener("click", flipCard);
+});
+}
